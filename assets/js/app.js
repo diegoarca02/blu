d3708cd90a935e3c6c098704c7e78d2b26e8447d
diegoarca0124 <155889956+diegoarca0124@users.noreply.github.com>
2025-01-27 14:55:10 -0500

    dsad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+
+
 function animateBars(){
     let bar_1 = document.querySelector('.icon-bar-1');
     let bar_2 = document.querySelector('.icon-bar-2');
@@ -6,4 +8,15 @@ function animateBars(){
     bar_1.classList.toggle('icon-bar-1-active');
     bar_2.classList.toggle('icon-bar-2-active');
     bar_3.classList.toggle('icon-bar-3-active');
+
+    const container = document.querySelector('.navbar-menu');
+    const isVisible = container.classList.contains('show');
+    
+    if (isVisible) {
+        container.classList.remove('show');
+        container.classList.add('hide');
+    } else {
+        container.classList.remove('hide');
+        container.classList.add('show');
+    }
 }
