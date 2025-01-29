f99c27b8df7ad95911bd67811c3b79626a0d8659
diegoarca0124 <155889956+diegoarca0124@users.noreply.github.com>
2025-01-29 13:42:08 -0500

    progressbars-1

--- assets/js/app.js
+++ assets/js/app.js
@@ -226,6 +226,41 @@ progressBars.forEach(bar => {
     });
 });
 
+gsap.utils.toArray("section").forEach((section, i) => {
+  section.bg = section.querySelector(".bg"); 
+
+  // Give the backgrounds some random images
+
+  // Do the parallax effect on each section
+  if (i) {
+    section.bg.style.backgroundPosition = `50% ${-innerHeight / 2}px`;
+
+    gsap.to(section.bg, {
+      backgroundPosition: `50% ${innerHeight / 2}px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        scrub: true
+      }
+    });
+  } 
+  
+  // the first image should be positioned against the top. Use px on the animating part to work with GSAP. 
+  else {
+    section.bg.style.backgroundPosition = "50% 0px"; 
+
+    gsap.to(section.bg, {
+      backgroundPosition: `50% ${innerHeight / 2}px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        start: "top top", 
+        end: "bottom top",
+        scrub: true
+      }
+    });
+  }
+});
 
 // Inicializar la animación del borde al cargar la página
 resetBorderRightAnimation();
